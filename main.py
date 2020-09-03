7a7450fe6c66dd5d36d0208c81bef279ca7cdc7f
[null] <48494379+asciid@users.noreply.github.com>
2020-09-03 22:52:54 +0300

    Documented some tricky moments in the code

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import protocol
 import os
 
 # TODO:
-# Better user interaction
-# Parallel processing
+# Optimise creating new webdriver copy for consistent links of the same type
+# Better (any) user interaction
+# Parallel processing (can it be mixed with the 1 item?)
 
 # DONE:
 # Tor support
@@ -17,6 +18,7 @@ import os
 # Built-in FTP checked out
 # Base error handling
 # Archiving
+# Tricky moments in code are documented
 
 app = Client(
         session_name='neoimager',
@@ -29,8 +31,11 @@ app = Client(
 def linkProcessing(client, message):
 
     def modulePick(link):
+        # [ http: ]/[ ]/[(example).(org)]/[whatever]
         domain = link.split('/')[2].split('.')[-1]
 
+        # This way makes expanding very easy
+        # Just write new config module for Firefox and apply it bellow 
         if domain == 'onion':
             profile = protocol.tor()
         elif domain == 'i2p':
@@ -41,16 +46,25 @@ def linkProcessing(client, message):
         return profile
 
     def archive(data, unix_time):
+        """
+        Takes data to write (to avoid passing webdriver into the function) and date
+        Returns absolute path of screenshot 
+        """
 
-        randomScreenShotName = lambda len: ''.join([chr(randint(65, 122)) for i in range(0, len)]) + '.png'
-        fileName = randomScreenShotName(10)
+        # 65 stands for the code of 'A'; 122 is for the 'z';
+        randomScreenshotName = lambda len: ''.join([chr(randint(65, 122)) for i in range(0, len)]) + '.png'
+        fileName = randomScreenshotName(10)
         unix_time = localtime(unix_time)
 
         YEAR = strftime('%Y', unix_time)
         MONTH = strftime('%m', unix_time)
         DAY = strftime('%d', unix_time)
 
-        for ent in ('archive', YEAR, MONTH, DAY): # Creating missing folders
+        # Folder structure:
+        # ./neoimager/archive/YEAR/MONTH/DAY/
+
+        # Create subfolder (if needed) and go deeper
+        for ent in ('archive', YEAR, MONTH, DAY):
             if not os.path.exists(ent): os.mkdir(ent)
             os.chdir(ent)
 
@@ -64,7 +78,6 @@ def linkProcessing(client, message):
         return out
 
     links = []
-    file_name = 'screenshot.png'
 
     if message.entities:
         for entity in message.entities:
@@ -75,7 +88,8 @@ def linkProcessing(client, message):
             #break #Add later if needed.
     
     for link in links:
-        
+        # Check of consistent links of the same proto ARE NEEDED!
+        # We call new browser for each link. It needs to be speed up.
         browser = webdriver.Firefox(firefox_profile=modulePick(link), executable_path='./geckodriver')
         err = ''
 
@@ -84,6 +98,7 @@ def linkProcessing(client, message):
         except common.exceptions.TimeoutException:
             err = '{}: Connection to the resource is timed out.\nTry again later.'.format(link)
         except common.exceptions.WebDriverException:
+            # This exception doesn't actually point at non-existent resource!
             err = '{}: Given resource does not exist.'.format(link)
 
         if err:
